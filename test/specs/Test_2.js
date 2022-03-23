9384fa2c6a7394eaa0f082d36fdff7b5e0f30204
Artur Sanko <sanartyrv@gmail.com>
2022-03-23 17:55:45 +0300

    Upload redo files

--- test/specs/Test_2.js
+++ test/specs/Test_2.js
@@ -6,37 +6,35 @@ describe('Test for task "Bring It On"', function () {
   const textHighlighting = 'Bash';
   const textPasteName = 'how to gain dominance among developers';
   const textPasteExpiration = '10 MIN';
-  const a = 'how to gain dominance among developers - Pastebin.com';
 
   const contentNewPaste = '//div[@class="source"]/child::ol';
   const contentSyntaxHighlighting = '//div[@class="left"]/child::a';
-  const contentPasteNameTitle =
-    '//title[normalize-space(text()) = "how to gain dominance among developers - Pastebin.com"]';
   const contentPasteExpiration = '//div[@class="expire"]';
+  const contentTitle = 'how to gain dominance among developers - Pastebin.com';
 
   it('have to "Открыть https://pastebin.com или аналогичный сервис в любом браузере"', async function () {
     await pastebin.openURL();
-    await browser.pause(2000);
+    await browser.pause(500);
   });
 
   it('have to "Создать New Paste со следующими деталями: * Код: git ..."', async function () {
     await pastebin.inputNewPaste(textNewPaste);
-    await browser.pause(2000);
+    await browser.pause(500);
   });
 
   it('have to "Создать New Paste со следующими деталями: * Syntax Highlighting: "Bash""', async function () {
     await pastebin.setSyntaxHighlightning(textHighlighting);
-    await browser.pause(2000);
+    await browser.pause(500);
   });
 
   it('have to "Создать New Paste со следующими деталями: * Paste Expiration: "10 Minutes""', async function () {
     await pastebin.setPasteExpiration();
-    await browser.pause(2000);
+    await browser.pause(500);
   });
 
   it('have to "Создать New Paste со следующими деталями: * Paste Name / Title: "how to gain dominance among developers""', async function () {
     await pastebin.inputPasteName(textPasteName);
-    await browser.pause(2000);
+    await browser.pause(500);
   });
 
   it('have to click the button "Create New Paste"', async function () {
@@ -44,29 +42,26 @@ describe('Test for task "Bring It On"', function () {
   });
 
   it('"New Paste" should contain valid value', async function () {
-    await browser.pause(2000);
+    await browser.pause(500);
     const elem = await $(contentNewPaste);
     await expect(elem).toHaveTextContaining(textNewPaste);
   });
 
   it('"Syntax Highlighting" should display valid value', async function () {
-    await browser.pause(2000);
+    await browser.pause(500);
     const elem = await $(contentSyntaxHighlighting);
     await expect(elem).toHaveTextContaining(textHighlighting);
   });
 
   it('"Paste Expiration" should display valid value', async function () {
-    await browser.pause(2000);
+    await browser.pause(500);
     const elem = await $(contentPasteExpiration);
     await expect(elem).toHaveTextContaining(textPasteExpiration);
   });
 
   it('Title of the page should display valid value', async function () {
-    await browser.pause(2000);
-    const title = browser.getTitle();
-    // const elem = await $(contentPasteNameTitle);
-    // const title = await elem.getText();
-    await expect(title).toHaveTextContaining(a);
+    await browser.pause(500);
+    await expect(browser).toHaveTitle(contentTitle);
   });
 });
 
