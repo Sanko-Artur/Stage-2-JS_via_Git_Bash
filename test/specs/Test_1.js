9c49505a1b4aec5def74aa72c1eee8b34040af50
Artur Sanko <sanartyrv@gmail.com>
2022-04-29 23:37:27 +0300

    redone Test_1.js

--- test/specs/Test_1.js
+++ test/specs/Test_1.js
@@ -4,19 +4,19 @@ describe('Test for task "I Can Win"', function () {
   const textNewPaste = 'Hello from WebDriver';
   const textPasteName = 'helloweb';
 
-  it('have to "Открыть https://pastebin.com или аналогичный сервис в любом браузере"', async function () {
+  it('have to "Open https://pastebin.com in any browser"', async function () {
     await pastebin.openURL();
   });
 
-  it('have to "Создать New Paste со следующими деталями: * Код: "Hello from WebDriver""', async function () {
+  it('have to "Create New Paste with following data: Hello from WebDriver"', async function () {
     await pastebin.inputNewPaste(textNewPaste);
   });
 
-  it('have to "Создать New Paste со следующими деталями: * Paste Expiration: "10 Minutes""', async function () {
+  it('have to "Create New Paste Expiration with following data: 10 Minutes"', async function () {
     await pastebin.setPasteExpiration();
   });
 
-  it('have to "Создать New Paste со следующими деталями: * Paste Name / Title: "helloweb""', async function () {
+  it('have to "Create New Paste Name / Title with following data: helloweb"', async function () {
     await pastebin.inputPasteName(textPasteName);
   });
 });
