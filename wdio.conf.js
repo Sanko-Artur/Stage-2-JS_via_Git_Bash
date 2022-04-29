193829a5760586c8869362fa59796f45fe91d43a
Artur Sanko <sanartyrv@gmail.com>
2022-04-29 23:31:59 +0300

    changed wdio.conf.js file

--- wdio.conf.js
+++ wdio.conf.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.config = {
-  specs: ['./test/specs/**/Test_2.js'],
+  specs: ['./test/specs/**/Test_1.js'],
 
   exclude: [
     // 'path/to/excluded/files'
